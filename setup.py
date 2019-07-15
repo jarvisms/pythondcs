b01980d94f95e925b8a657fbe5f95521500fc3ce
Mark Jarvis <43512302+jarvisms@users.noreply.github.com>
2019-07-15 14:11:05 +0100

    Committing current version to PyPi
    Release 0.1.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="pythondcs",
-    version="0.1.0",
+    version="0.1.1",
     author="Mark Jarvis",
     description="Python Module for interfacing with the Coherent Research DCS v3+ remote metering server",
     long_description=long_description,
